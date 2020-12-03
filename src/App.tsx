e7368570a6f78d4eb092f1cdf96cd382ab66d3a3
Andre Sbrocco <andresbrocco@gmail.com>
2020-12-03 15:33:27 +0100

    Initial setup for React, webpack, TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+	<div className="App" >
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio debitis odit, qui similique, nulla repudiandae, labore voluptas corporis tempore animi architecto reiciendis facilis possimus minus. Maxime quos et veritatis deleniti!</p>
+	</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+const config = {
+	mode: isProd ? "production" : "development",
+	entry: {
+		index: "./src/index.tsx",
+	},
+	devtool: "inline-source-map",
+	module: {
+			rules: [
+					{ 
+							test: /\.tsx?$/,
+							exclude: /node_modules/,
+							use: 'babel-loader'
+					},
+					{
+							test: /\.css$/,
+							use: ["style-loader", "css-loader"]
+					}
+			]
+	},
+	resolve: {
+		extensions: ['*', '.ts', '.tsx', '.js', '.jsx']
+	},
+	output: {
+			path: path.resolve(__dirname, 'dist'),
+			publicPath: '/',
+			filename: 'bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+			filename: "index.html",
+			inject: "body"
+		})
+	]
+};
+
+if (isProd) {
+	config.optimization = {
+		minimizer: [new TerserWebpackPlugin()]
+	};
+} else {
+	config.devServer = {
+		contentBase: path.join(__dirname, 'public/'),
+		publicPath: 'http://localhost:3000/',
+		hotOnly: true,
+		writeToDisk: true,
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		stats: "errors-only",
+		overlay: true
+	};
+};
+
+module.exports = config;
