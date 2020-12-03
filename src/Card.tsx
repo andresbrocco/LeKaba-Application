da6fe4a9edc8599c5c8ecad64b4e503cd5584cdb
Andre Sbrocco <andresbrocco@gmail.com>
2020-12-04 00:39:06 +0100

    added react-bootstrap carousel

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
-import Carousel from './Carousel';
 import IPlace from './Place';
+import Carousel from 'react-bootstrap/Carousel';
 
 interface ICard {
 	key: number;
@@ -9,10 +9,18 @@ interface ICard {
 }
 
 function Card(props: ICard) {
+	var images:string[] = props.place.images.split("|");
 	return (
 		<div className="Card">
 			<h4>{props.place.name}</h4>
-			<Carousel images={props.place.images} name={props.place.name}/>
+			<Carousel indicators={false} interval={null}>
+				{images.map(image => 
+					<Carousel.Item key={image}>
+						{console.log(image)}
+						<img src={image} alt={props.place.name} className="CarouselImage"/>
+					</Carousel.Item>
+				)}
+			</Carousel>
 			<p>{props.place.address}</p>
 		</div>
 	)
