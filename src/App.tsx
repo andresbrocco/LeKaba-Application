0d18c265945c8a99e0b3beea39574b0b11ac2aaa
Andre Sbrocco <andresbrocco@gmail.com>
2020-12-04 01:29:34 +0100

    added google maps api.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 			</header>
 			<section className="Body">
 				<Grid places={csvData}/>
-				<MyMap />
+				<MyMap places={csvData}/>
 			</section>
 		</div>
 	);
--- src/MyMap.tsx
+++ src/MyMap.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './MyMap.css';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import IPlace from './Place';
+var googleAPIKey = "";
 
-function MyMap() {
+interface IMyMap {
+	places: IPlace[];
+}
+
+function MyMap(props: IMyMap) {
+	const mapStyles = {
+		height: "100%",
+		width: "100%"
+	};
+	var LatSum=0;
+	var LngSum=0;
+	var n_places=0;
+	props.places.map(place => {
+		n_places += 1;
+		LatSum += place.latitude;
+		LngSum += place.longitude;
+	})
+	const defaultCenter = {
+		// Paris coordinates:
+		lat: LatSum/n_places,
+		lng: LngSum/n_places
+  }
 	return (
 		<div className="MyMap">
-			My Map content
+			<LoadScript
+				googleMapsApiKey={googleAPIKey}>
+				<GoogleMap
+					mapContainerStyle={mapStyles}
+					zoom={13}
+					center={defaultCenter}
+				>
+				{
+					props.places.map(place => {
+						return (
+						<Marker key={place.name} position={{lat: place.latitude, lng: place.longitude}}/>
+						)
+					})
+				}
+				</GoogleMap>
+			</LoadScript>
 		</div>
 	)
 };
